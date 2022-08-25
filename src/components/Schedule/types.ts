140838be743ae780b4d6a6f3369f9a97a1c3cc57
Utrumque <frankenstain1980@gmail.com>
2022-08-25 23:58:15 +0300

    first commit

---
+++ src/components/Schedule/types.ts
@@ -0,0 +1,18 @@
+export interface ISchedule {
+	time: String
+	gasPrice: Number
+	gasValue: Number
+	average: Number
+	maxGasPrice: Number
+	medianGasPrice: Number
+
+	//time: Number
+	//high: Number
+	//low: Number
+	//open: Number
+	//volumefrom: Number
+	//volumeto: Number
+	//close: Number
+	//conversionType: String
+	//conversionSymbol: String
+}
